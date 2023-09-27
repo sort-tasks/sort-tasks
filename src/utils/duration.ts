6819f6ef6685ffb78bc19836636461b7bca0ebd9
David Costa <davidcostadev@gmail.com>
2023-09-27 13:53:14 -0300

    Improve the duration label

--- src/utils/duration.ts
+++ src/utils/duration.ts
@@ -9,23 +9,23 @@ export function durationToNow(isoDateTime: string) {
   const result: string[] = [];
 
   if (duration.months > 0) {
-    result.push(`${duration.months.toFixed(0)} month${duration.months > 1 ? 's' : ''}`);
+    result.push(`${duration.months.toFixed(0)} month${parseInt(`${duration.months}`, 10) !== 1 ? 's' : ''}`);
   }
 
   if (duration.weeks > 0) {
-    result.push(`${duration.weeks.toFixed(0)} week${duration.weeks > 1 ? 's' : ''}`);
+    result.push(`${duration.weeks.toFixed(0)} week${parseInt(`${duration.weeks}`, 10) !== 1 ? 's' : ''}`);
   }
 
   if (duration.days > 0) {
-    result.push(`${duration.days.toFixed(0)} day${duration.days > 1 ? 's' : ''}`);
+    result.push(`${duration.days.toFixed(0)} day${parseInt(`${duration.days}`, 10) !== 1 ? 's' : ''}`);
   }
 
   if (duration.hours > 0) {
-    result.push(`${duration.hours.toFixed(0)} hour${duration.hours > 1 ? 's' : ''}`);
+    result.push(`${duration.hours.toFixed(0)} hour${parseInt(`${duration.hours}`, 10) !== 1 ? 's' : ''}`);
   }
 
   if (duration.minutes > 0) {
-    result.push(`${duration.minutes.toFixed(0)} minute${duration.minutes > 1 ? 's' : ''}`);
+    result.push(`${duration.minutes.toFixed(0)} minute${parseInt(`${duration.minutes}`, 10) !== 1 ? 's' : ''}`);
   }
 
   return result.slice(0, 2).join(' and ');
