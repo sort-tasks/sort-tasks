ac9c1a030f17cfe01a62240de3f3bcd8e52ceb76
David Costa <davidcostadev@gmail.com>
2023-10-04 11:02:36 -0300

    Fix scroll on the body

--- src/components/modals/ViewTaskModal.tsx
+++ src/components/modals/ViewTaskModal.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import { DateTime } from 'luxon';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 import { toast } from 'react-toastify';
 
@@ -223,10 +223,27 @@ type ModalWrapperProps = {
 };
 
 export const ModalWrapper = ({ children, isOpen, onRequestClose }: ModalWrapperProps) => {
-  const overlayClassName = clsx('fixed inset-0 z-50 flex items-start justify-center bg-black/20 backdrop-blur-sm');
-  const contentClassName = clsx(
-    'bg-background mt-20 w-full max-w-screen-lg border-y border-on-background/10 md:mx-4 md:rounded-2xl md:border-x',
+  const overlayClassName = clsx(
+    'fixed inset-0 z-50 flex items-start justify-center overflow-y-auto bg-black/20 backdrop-blur-sm',
   );
+  const contentClassName = clsx(
+    'bg-background mb-10 mt-20 w-full max-w-screen-lg border-y border-on-background/10 outline-none md:mx-4 md:rounded-2xl md:border-x',
+  );
+
+  useEffect(() => {
+    console.log(isOpen);
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => (document.body.style.overflow = 'unset');
+  }, []);
+
   return (
     <Modal
       isOpen={isOpen}
